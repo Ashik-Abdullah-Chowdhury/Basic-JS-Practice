9f9666ce5080f62f41c7e74809de8f462ebfb986
Ashik Abdullah <157312602+Ashikkkkkkk@users.noreply.github.com>
2024-02-26 16:22:10 +0600

    uploaded first file

---
+++ basic-1.js
@@ -0,0 +1,19 @@
+Num2=344
+txtNum="344"
+console.log(Num2===txtNum)
+
+
+var firstname="Ashik"
+var lastname=" Abdullah"
+var fullname=firstname+lastname
+console.log(fullname)
+
+
+Num1 = 59
+txt1 = "hello JS"
+console.log(Num1===txt1)
+
+
+var a = 5
+var b = "60"
+console.log(a+Number(b))
