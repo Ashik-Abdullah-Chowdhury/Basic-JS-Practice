5e30fc56c2d5e7a5e2c2e559d4ac4d8f7dac318d
Ashik Abdullah <157312602+Ashikkkkkkk@users.noreply.github.com>
2024-03-01 17:26:47 +0600

    Update basic-5.js

--- basic-5.js
+++ basic-5.js
@@ -1,31 +1,31 @@
-// function summation(a,b){
-//     return a+b
-// }
-// function subtraction(c,d){
-//     return c-d
-// }
-// function multiplication(e,f){
-//     return e*f
-// }
-// function calculation(num1,num2,summation,subtraction,multiplication){
-// var x=summation(num1,num2)
-// var y=subtraction(num1,num2)
-// var z=multiplication(num1,num2)
-// console.log("summation is: ",x,"\n","subtraction is: ",y,"\n","multiplication is: ",z)
-// return x,y,z
-// }
-// var prompt=require('prompt-sync')()
-// var num1=Number(prompt("enter the first number: "))
-// var num2=Number(prompt("enter the second number: "))
-// calculation(num1,num2,summation,subtraction,multiplication)
+function summation(a,b){
+    return a+b
+}
+function subtraction(c,d){
+    return c-d
+}
+function multiplication(e,f){
+    return e*f
+}
+function calculation(num1,num2,summation,subtraction,multiplication){
+var x=summation(num1,num2)
+var y=subtraction(num1,num2)
+var z=multiplication(num1,num2)
+console.log("summation is: ",x,"\n","subtraction is: ",y,"\n","multiplication is: ",z)
+return x,y,z
+}
+var prompt=require('prompt-sync')()
+var num1=Number(prompt("enter the first number: "))
+var num2=Number(prompt("enter the second number: "))
+calculation(num1,num2,summation,subtraction,multiplication)
 
 
-// function  square(x){
-//     return x**2
-//   }
-//   ar1=[1,2,3,4]
-//   ar2=ar1.map(square)
-//   console.log(ar2)
+function  square(x){
+    return x**2
+  }
+  ar1=[1,2,3,4]
+  ar2=ar1.map(square)
+  console.log(ar2)
 
  
   function login(name="guest"){
@@ -36,9 +36,9 @@
   login()
 
  
-//   function a(x){
-//     var Y=x**2+2*x+6
-//     console.log(Y)
-//     return Y
-//     }
-//     a(2)
+  function a(x){
+    var Y=x**2+2*x+6
+    console.log(Y)
+    return Y
+    }
+    a(2)
