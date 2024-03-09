52b8b7f8065254151939698956602fc708493b91
Ashik Abdullah Chowdhury <157312602+Ashik-Abdullah-Chowdhury@users.noreply.github.com>
2024-03-10 01:48:28 +0600

    updated

--- basic-1.js
+++ basic-1.js
@@ -1,19 +1,26 @@
+// Write code:Num2 = 344, txtNum = "344". Compare the value of both variables along with the data type and print the result.
 Num2=344
 txtNum="344"
 console.log(Num2===txtNum)
+console.log(typeof(Num2))
+console.log(typeof(txtNum))
 
 
+// Declare two variables, then assign your first name and last name. Finally, print your full name including one space between both names.
 var firstname="Ashik"
 var lastname=" Abdullah"
 var fullname=firstname+lastname
 console.log(fullname)
 
 
+// Num1 = 59, txt1 = "hello JS". Compare both variables and print the result.
 Num1 = 59
 txt1 = "hello JS"
 console.log(Num1===txt1)
+console.log(typeof(Num1))
+console.log(typeof(txt1))
 
-
+// Suppose a = 5, b = "60". Use type conversion and print the sum of the two variables.
 var a = 5
 var b = "60"
 console.log(a+Number(b))
